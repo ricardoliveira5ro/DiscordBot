4dd6e0458ac048f7364e15e9bd5d948dddb04818
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-05-06 16:14:05 +0100

    !start command

--- src/bot.js
+++ src/bot.js
@@ -19,6 +19,8 @@ client.on('ready', (c) => {
 const COMMAND_PREFIX = '!';
 const { generateRandomWord } = require('./words');
 
+const games = {};
+
 client.on('messageCreate', (message) => {
     let args = message.content.substring(COMMAND_PREFIX.length).split(" ");
 
@@ -28,12 +30,37 @@ client.on('messageCreate', (message) => {
             const helpEmbed = {
                 color: 0x0099ff,
                 title: 'Guess the word!',
-                description: 'Type `!start` to begin the game. A new word will be randomized and you can start guessing by hinting a letter with command `!letter [letter]`, or eventually guessing the word typing `!guess [word]`.',
+                description: 'Type `!start` to begin the game. A new word will be randomized and you can start guessing by hinting a letter with command `!letter [letter]`, or eventually guessing the word typing `!guess [word]`\n\nUse `!stop` to stop an ongoing game or surrender.',
                 footer: {
                     text: 'Enjoy the game!',
                 },
             };
             message.reply({ embeds: [helpEmbed] });
+            break;
+
+        case 'start':
+            if (!games[message.channel.id]) {
+                const word = generateRandomWord();
+
+                games[message.channel.id] = {
+                    word: word,
+                    guesses: [],
+                    triesLeft: 6 //to be adjusted
+                }
+
+                //To be removed
+                console.log('the game started')
+                message.channel.send(word)
+
+            } else {
+                const messageEmbed = {
+                    color: 0xFF0000,
+                    title: 'Could not start the game!',
+                    description: 'Unfortunately there is a game ongoing, to start a new game you have to finish this one first. You could also surrender or cancel the game using command `!stop`'
+                };
+
+                message.reply({ embeds: [messageEmbed] });
+            }
             break;
     }
 });
