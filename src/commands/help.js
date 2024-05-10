f1343bcbed492ba91460521c675f26287b7ac1f9
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-05-11 00:10:25 +0100

    !help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,6 +1,14 @@
+const { botMessage, tooManyArgumentsMessage } = require("../utils/messageHandler");
+
 module.exports = {
     name: "help",
     execute(message, args) {
-        message.channel.send("Help command!");
+        if(args[0]) { 
+            message.reply({ embeds: [tooManyArgumentsMessage] });
+            return; 
+        }
+
+        const helpMessage = botMessage(0x0099FF, 'Guess the word!', 'Type `!start` to begin the game. A new word will be randomized and you can start guessing by hinting a letter with command `!letter [letter]`, or eventually guessing the word typing `!guess [word]`\n\nUse `!stop` to stop an ongoing game or surrender.', '');
+        message.reply({ embeds: [helpMessage] });
     },
 };
